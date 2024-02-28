a8f7fd48575e32d4e99df046c9e13181e3a23cf6
AnandVihar143 <148607169+AnandVihar143@users.noreply.github.com>
2024-02-28 23:09:09 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,10 +1,10 @@
 var count = 1;
-document.getElementById("img").style.backgroundImage=`url(../ASSETS/${1}.jpg)`
+document.getElementById("img").style.backgroundImage=`url(./ASSETS/${1}.jpg)`
 
 function add()
 {
     count++;
-    document.getElementById("img").style.backgroundImage=`url(../ASSETS/${count}.jpg)`
+    document.getElementById("img").style.backgroundImage=`url(./ASSETS/${count}.jpg)`
     console.log(count);
     if(count == 2)
     {
